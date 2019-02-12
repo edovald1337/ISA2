a25927b76ba55a7715935130b9a1a2a06c2c68f4
Raul Madis Edovald <raedov@ttu.ee>
2019-02-13 01:56:51 +0200

    c

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,32 +10,45 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Bob's big giveaway!");
             Console.Write("Choose a door: 1, 2 or 3: ");
             string userValue = Console.ReadLine();
 
+            string message = "";
+
             if (userValue == "1")
-            {
-                string message = "You won a new car!";
-                Console.WriteLine(message);
-            }
+                message = "You won a new car!";
             else if (userValue == "2")
-            {
-                string message = "You won a new boat!";
-                Console.WriteLine(message);
-            }
+                message = "You won a new boat!";
             else if (userValue == "3")
-            {
-                string message = "You won a new cat!";
-                Console.WriteLine(message);
-            }
+                message = "You won a new cat!";
             else
             {
-                string message = "Sorry, we didn't understand.";
-                Console.WriteLine(message);
+                message = "Sorry, we didn't understand. ";
+                //message = message + "You Lose.";
+                message += "You Lose.";
             }
+                
+            Console.WriteLine(message);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("Bob's big giveaway!");
+            Console.Write("Choose a door: 1, 2 or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            //Console.Write("You won a ");
+            //Console.Write(message);
+            //Console.Write(".");
+
+            //Console.WriteLine("You won a {0}.",message);
+
+            Console.WriteLine("You entered: {0}, therefore you won a {1}", userValue, message);
 
             Console.ReadLine();
+
         }
     }
 }
