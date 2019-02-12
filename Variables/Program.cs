de2ed7289516e48a7c0cf173c756610bb829469a
Raul Madis Edovald <raedov@ttu.ee>
2019-02-12 22:26:53 +0200

    c

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -11,11 +11,23 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            /*
             int x;
             int y;
             x = 7;
             y = x + 3;
             Console.WriteLine(y);
+            Console.ReadLine();
+            */
+
+            Console.WriteLine("What is your name?");
+            Console.Write("First name: ");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+            string myLastName;
+            Console.Write("Last name: ");
+            myLastName = Console.ReadLine();
+            Console.WriteLine("Hello, " + myFirstName + " " + myLastName);
             Console.ReadLine();
         }
     }
